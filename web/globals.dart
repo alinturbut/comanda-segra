36686c8dcc86f3925588d5f7870ab24f6234207e
Cristian Popa <cristian.popa@voquz.com>
2016-11-16 13:34:19 +0200

    Created post request for order

---
+++ web/globals.dart
@@ -0,0 +1,5 @@
+final segraUrl = 'http://www.salanunta.ro/catering-zilnic.html';
+
+final segraOrderUrl = 'http://www.salanunta.ro/catering-zilnic.html?action=send';
+
+final segraDetailsUrl = 'http://www.salanunta.ro/catering-zilnic.comanda.html';
